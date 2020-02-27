460afdbf17920f8103af927d66656ab18a51d776
Vojtech Miksu <vojtech@uber.com>
2020-02-26 16:36:06 -0800

    fix base web

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,15 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-
-import React from 'react';
-import { BaseProvider } from 'baseui';
-
-
-export const wrapRootElement = ({ element }) => {
-  return (
-    <BaseProvider id="baseui">
-      {element}
-    </BaseProvider>
-  );
-};
